400c80432978cc948909ef2fa76ff873003796dc
Sigma-One <sigmaone42@gmail.com>
2020-08-11 02:31:33 +0300

    Add mode colours to screwdriver

--- src/main/java/sigmaone/industrialism/item/ItemScrewdriver.java
+++ src/main/java/sigmaone/industrialism/item/ItemScrewdriver.java
@@ -3,7 +3,6 @@ package sigmaone.industrialism.item;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -23,10 +22,12 @@ public class ItemScrewdriver extends Item {
             if (this.opposite) {
                 this.opposite = false;
                 user.sendMessage(new TranslatableText("item."+ Industrialism.MOD_ID+".screwdriver.popup.config_normal"), true);
+                user.getStackInHand(hand).getOrCreateTag().putInt("CustomModelData", 0);
             }
             else {
                 this.opposite = true;
                 user.sendMessage(new TranslatableText("item."+ Industrialism.MOD_ID+".screwdriver.popup.config_opposite"), true);
+                user.getStackInHand(hand).getOrCreateTag().putInt("CustomModelData", 1);
             }
         }
         return new TypedActionResult<>(ActionResult.SUCCESS, user.getStackInHand(hand));
