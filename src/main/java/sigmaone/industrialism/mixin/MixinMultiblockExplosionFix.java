0ab7bf531b8b5037eebd0b1df406025f2477d31e
Sigma-One <Sigma1@tuta.io>
2020-10-17 21:39:06 +0300

    Multiblock breaking on explosions

---
+++ src/main/java/sigmaone/industrialism/mixin/MixinMultiblockExplosionFix.java
@@ -0,0 +1,39 @@
+package sigmaone.industrialism.mixin;
+
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+import sigmaone.industrialism.block.multiblock.BlockEntityMultiblockChildBase;
+import sigmaone.industrialism.block.multiblock.BlockEntityMultiblockRootBase;
+
+import java.util.Iterator;
+
+@Mixin(Explosion.class)
+public class MixinMultiblockExplosionFix {
+    @Shadow @Final private World world;
+
+    @Inject(method="affectWorld", at=@At(
+            value="INVOKE",
+            target="Lnet/minecraft/world/World;setBlockState(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;I)Z",
+            ordinal=0
+    ), locals=LocalCapture.CAPTURE_FAILHARD)
+    private void callDisassemble(boolean bl, CallbackInfo ci, boolean bl2, ObjectArrayList objectArrayList, Iterator var4, BlockPos blockPos, BlockState blockState, Block block) {
+        if (this.world.getBlockEntity(blockPos) instanceof BlockEntityMultiblockRootBase) {
+            ((BlockEntityMultiblockRootBase) this.world.getBlockEntity(blockPos)).disassemble();
+        }
+        else if (this.world.getBlockEntity(blockPos) instanceof BlockEntityMultiblockChildBase) {
+            BlockPos parent = ((BlockEntityMultiblockChildBase) this.world.getBlockEntity(blockPos)).getParent();
+            ((BlockEntityMultiblockRootBase) this.world.getBlockEntity(parent)).disassemble();
+        }
+    }
+}
